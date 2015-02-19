3ad32f2d7f9e5aa96a6b3f86df37331efa2abcbf
spygi <spygiannakis@hotmail.com>
2015-02-19 21:21:15 +0100

    threshold more or less working

--- app.sh
+++ app.sh
@@ -1,16 +1,36 @@
 #!/bin/sh
+# This script will help you calculate your lunch break.
+# 
+# Specifically: writes wake up time in the beginning of the day.
+# Sleep time if during lunch time or end-of-day times and break was big enough.
+#
+# Why not check also other times? 
+# Because of meetings or discussions were laptop might sleep but you are still working
+
+isItLunchOrNightTime() {
+	local LUNCHSTART="11:30:00"
+	local LUNCHSTOP="13:30:00"
+	local NIGHTSTART="17:00:00"
+	local NIGHTSTOP="23:59:59"
+	if [[ $(( $1 - `date -j -f "%T" "$LUNCHSTART" "+%s"` )) -ge 0 && $(( $1 - `date -j -f "%T" "$LUNCHSTOP" "+%s"` )) -le 0 ]] 
+	then
+		echo true
+	elif [[ $(( $1 - `date -j -f "%T" "$NIGHTSTART" "+%s"` )) -ge 0 && $(( $1 - `date -j -f "%T" "$NIGHTSTOP" "+%s"` )) -le 0 ]] 
+	then 
+		echo true
+	fi
+}
 
 # enter subshell so we don't pollute with variables
 (  
 # Settings #
 FILE=time.csv
-LUNCHTIME="11:30:00-13:30:00"
-THRESHHOLD=`echo 10*60 | bc` # 10 minutes
+THRESHHOLD=$(( 1*60 )) # 10 minutes
 TIMESEPARATOR=";" 
 
 # Variables #
-TODAY=`date "+%Y-%m-%d"`
-TIME=`date "+%H:%M:%S"`
+TODAY=`date "+%F"` # +%Y-%m-%d
+TIME=`date "+%T"` # +%H:%M:%S
 NOW=`date +%s` # timestamp
 
 [ ! -f $FILE ] && echo "File $FILE not found!\n Creating it now!" && touch $FILE
@@ -20,17 +40,27 @@ ALREADYLOGGEDINTODAY=`grep $TODAY $FILE`
 LASTSLEEPTIME=`tail -n 1 $FILE | awk -F "$TIMESEPARATOR" '{if (NF%2 == 0) {print $NF}}'` 
 if [ ! -z "$LASTSLEEPTIME" ]
 then
-	LASTSLEEPTIMESTAMP=`date -j -f "%H:%M:%S" "${LASTSLEEPTIME}" "+%s"`
+	LASTSLEEPTIMESTAMP=`date -j -f "%T" "${LASTSLEEPTIME}" "+%s"`
 fi
 
 if [ -z "$ALREADYLOGGEDINTODAY" ]
 then
 	# It's a new day, you look great today
 	printf "%s %s" "$TODAY" "$TIME" > $FILE
-elif [[ -z $LASTSLEEPTIME || $(($NOW - $LASTSLEEPTIMESTAMP)) -gt $THRESHHOLD ]]
+elif [[ (-z $LASTSLEEPTIME && "`isItLunchOrNightTime $NOW`" = true) ]]  
 then
-	# We are either sleeping now or
-	# we are waking up from a long enough sleep
-	printf "%s%s" "$TIMESEPARATOR" "$TIME" >> $FILE
+	echo 1 
+	# We are sleeping now while lunch time
+	sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE
+	# printf "%s%s" "$TIMESEPARATOR" "$TIME" >> $FILE
+elif [[ $(( $NOW - $LASTSLEEPTIMESTAMP )) -lt THRESHHOLD ]]
+then 
+	echo 2
+	# Was not big enough to be considered a (lunch or night) break
+	sed -i '' '$ s/'$TIMESEPARATOR$LASTSLEEPTIME'//' $FILE
+else 
+	echo 3
+	# we are waking up after the lunch break
+	sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE
 fi
 )
