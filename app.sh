8c20f85a167a08080b426398566efe4e3ace9e7e
spygi <spygiannakis@hotmail.com>
2015-02-19 19:06:38 +0100

    we write every sleep, we should check if it's during lunch

--- app.sh
+++ app.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# enter subshell so we don't pollute with variables
+(  
 # Settings #
 FILE=time.csv
 LUNCHTIME="11:30:00-13:30:00"
@@ -12,15 +14,12 @@ TIME=`date "+%H:%M:%S"`
 NOW=`date +%s` # timestamp
 
 [ ! -f $FILE ] && echo "File $FILE not found!\n Creating it now!" && touch $FILE
-
 # TODO: check format of file (eg first line is of the correct format)
 
 ALREADYLOGGEDINTODAY=`grep $TODAY $FILE`
 LASTSLEEPTIME=`tail -n 1 $FILE | awk -F "$TIMESEPARATOR" '{if (NF%2 == 0) {print $NF}}'` 
-if [ -z "$LASTSLEEPTIME" ]
-then 
-	NEWDAYORWILLSLEEPNOW=1
-else
+if [ ! -z "$LASTSLEEPTIME" ]
+then
 	LASTSLEEPTIMESTAMP=`date -j -f "%H:%M:%S" "${LASTSLEEPTIME}" "+%s"`
 fi
 
@@ -28,10 +27,10 @@ if [ -z "$ALREADYLOGGEDINTODAY" ]
 then
 	# It's a new day, you look great today
 	printf "%s %s" "$TODAY" "$TIME" > $FILE
-elif [[ ! -z $NEWDAYORWILLSLEEPNOW || $(($NOW - $LASTSLEEPTIMESTAMP)) -gt $THRESHHOLD ]]
+elif [[ -z $LASTSLEEPTIME || $(($NOW - $LASTSLEEPTIMESTAMP)) -gt $THRESHHOLD ]]
 then
+	# We are either sleeping now or
+	# we are waking up from a long enough sleep
 	printf "%s%s" "$TIMESEPARATOR" "$TIME" >> $FILE
 fi
-
-unset LASTSLEEPTIME
-unset NEWDAYORWILLSLEEPNOW
+)
