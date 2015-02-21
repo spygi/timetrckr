970f328938ba01cb0c26e2e92c1e6a52ec5977c0
spygi <spygiannakis@hotmail.com>
2015-02-21 15:54:39 +0100

    fixed some bugs, parsing of file + os x notifications

--- app.sh
+++ app.sh
@@ -31,17 +31,22 @@ lastWorkingDay() {
 	fi
 }
 
-# This won't work if you run the script from tmux
+# OSX notifications won't work if you run the script from tmux
 showSummary() {
 	local LINE=`grep "$LASTWORKINGDAY" $FILE`
-	local DIFF=0
-	if [[ ! -z $LINE ]]
+	if [[ ! -z LINE ]] 
 	then
-		#local NR=`cat $LINE | awk -F "$TIMESEPARATOR" '{print NR}'` # '{diff=0; cmd="date -j -f \"%T\" \"+%s\" "; for(i=0;i<NR;i++) { wake=cmd | $1; sleep=cmd } }'
-		#while IFS=$TIMESEPARATOR read -ra line; do
-		#	$((${line[1]} && echo "${line[1]}" | openssl dgst -sha1
-		#done < inputFile
-		#`osascript -e 'display notification "Lorem ipsum dolor sit amet" with title "Title"'`
+		# convert the line to timestamps and calculate the diff
+		# Notes:
+		# the space is required for [ $TIMESEPARATOR] since the date has a space
+		# $0 is the full line in awk and $1 is the date, that's why we start the loop with $2
+		# NR starts from 1 in awk	
+		local DIFF=`echo $LINE | awk -F "[ $TIMESEPARATOR]" '{for (i=2; i<=NF;i++) {print $i}}' | xargs -n1 date -j -f "%T" "+%s" | awk 'BEGIN{diff=0;}{if(NR%2==0){diff+=$0} else {diff-=$0}} END{print diff}'`
+		# TODO if diff < 0 something's wrong
+		
+		local TEXT=$((DIFF/60/60))"hours "$(((DIFF%60)/60))"minutes"
+		osascript -e "display notification \"worked on $LASTWORKINGDAY\" with title \"$TEXT\""
+		# TODO: save it somewhere
 	fi
 }
 
@@ -49,17 +54,14 @@ showSummary() {
 (  
 # Settings #
 FILE=time.csv
-THRESHHOLD=$(( 1*60 )) # 10 minutes
+THRESHHOLD=$(( 10*60 )) # 10 minutes
 TIMESEPARATOR=";" 
+LASTWORKINGDAY=`lastWorkingDay`
 
 # Variables #
 TODAY=`date "+%F"` # +%Y-%m-%d
 TIME=`date "+%T"` # +%H:%M:%S
 NOW=`date +%s` # timestamp
-LASTWORKINGDAY=`lastWorkingDay`
-
-
-
 
 [ ! -f $FILE ] && echo "File $FILE not found!\n Creating it now!" && touch $FILE
 # TODO: check format of file (eg first line is of the correct format)
@@ -74,24 +76,24 @@ fi
 if [ -z "$ALREADYLOGGEDINTODAY" ]
 then
 	# It's a new day
-	printf "%s %s" "$TODAY" "$TIME" > $FILE
+	printf "%s %s" "$TODAY" "$TIME" >> $FILE
 	
 	# run the summary for the lastWorkingDay
-	if [[ `grep "$LASTWORKINGDAY"`]]
+	showSummary
 elif [[ (-z $LASTSLEEPTIME && "`isItLunchOrNightTime $NOW`" = true) ]]  
 then
-	echo 1 
 	# We are sleeping now while lunch time
-	sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE
-	# printf "%s%s" "$TIMESEPARATOR" "$TIME" >> $FILE
-elif [[ $(( $NOW - $LASTSLEEPTIMESTAMP )) -lt THRESHHOLD ]]
+	sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE # printf "%s%s" "$TIMESEPARATOR" "$TIME" >> $FILE
+elif [[ ! -z $LASTSLEEPTIME ]]
 then 
-	echo 2
-	# Was not big enough to be considered a (lunch or night) break
-	sed -i '' '$ s/'$TIMESEPARATOR$LASTSLEEPTIME'//' $FILE
-else 
-	echo 3
-	# we are waking up after the lunch break
-	sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE
+	# We are waking up again
+	if [[ $(( $NOW - $LASTSLEEPTIMESTAMP )) -lt THRESHHOLD ]]
+	then
+		# Was not big enough to be considered a (lunch or night) break
+		sed -i '' '$ s/'$TIMESEPARATOR$LASTSLEEPTIME'//' $FILE
+	else 
+		# write a new wake time
+		sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE
+	fi
 fi
 )
