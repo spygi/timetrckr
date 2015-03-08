641299c7b0ff892a38b44028a784ee25b6f12cf8
spygi <spygiannakis@hotmail.com>
2015-03-08 23:34:46 +0100

    simplified logic + validation, disabled summary for the time being. Tested.

--- timetrckr.sh
+++ timetrckr.sh
@@ -64,8 +64,8 @@ WAKESTATE="wake"
 
 # Default settings #
 CONFFILE=$APPNAME".conf" 
-FILE=~/time.csv
-OUTPUTFILE=~/summary.csv
+FILE=time.csv
+OUTPUTFILE=summary.csv
 THRESHOLD=$(( 10*60 )) # 10 minutes
 TIMESEPARATOR=";" 
 LUNCHSTART="11:30:00"
@@ -119,80 +119,80 @@ done < $CONFFILE
 
 # Main part #
 [ ! -f $FILE ] && logger -t $APPNAME "File $FILE not found, creating it now" && touch $FILE
-# TODO: check format of file (eg first line is of the correct format)
 
-LASTWORKINGDAY=`tail -n 1 $FILE | awk -F " " '{print $1}'`
 ALREADYLOGGEDINTODAY=`grep $TODAY $FILE`
-if [[ -z $ALREADYLOGGEDINTODAY]] 
-then 
-	# validation 
-	if [[ $STATE ne $WAKESTATE ]]
-	then
-		osascript -e "display notification \"State is $STATE but there is no wake event for today. Please check your $FILE\" with title \"$APPNAME\""
-		exit 1;
-	fi 
-
-	LASTSHUTDOWNTIMESTAMP=`tail -r /private/var/log/system.log | grep -m 1 $SHUTDOWNPATTERN | awk -F "$SHUTDOWNPATTERN" '{print $2}' | awk '{print $1}'`
-	# an alternative way to do this would be through last shutdown | head -n 1 but too slow and the format is not suitable for date to parse
-	if [ -z $LASTSHUTDOWNTIMESTAMP ]
-	then 
-		osascript -e "display notification \"No shutdown time was found, please insert it manually in $FILE\" with title \"$APPNAME\""
-		sleep 2 # before showing possibly another notification
-	else 
-		logger -t $APPNAME "Writing last shutdown time to $FILE"
-		LASTSHUTDOWNTIME=`date -j -f "%s" $LASTSHUTDOWNTIMESTAMP "+%T"` 
-		sed -i '' '$ s/$/'$TIMESEPARATOR$LASTSHUTDOWNTIME'/' $FILE
-	fi
-
-	logger -t $APPNAME "Starting a new day"
-	printf "%s %s" "$TODAY" "$TIME" >> $FILE
-
-	if [[ ! -z $LASTWORKINGDAY ]]
-	then
-		sleep 2 # give some time for the notifications
-		# Create results for previous working day	
-		showSummary
-	else
-		# the script starts with a fresh FILE
-		osascript -e "display notification \"$APPNAME has started recording...\" with title \"Ahoy!\""
-	fi
-
-	exit 0;
-fi
-
 LASTSLEEPTIME=`tail -n 1 $FILE | awk -F "$TIMESEPARATOR" '{if (NF%2 == 0) {print $NF}}'` # NF is even
-if [ ! -z "$LASTSLEEPTIME" ]
-then
-	LASTSLEEPTIMESTAMP=`date -j -f "%T" "${LASTSLEEPTIME}" "+%s"`
-	
-	if [[ $STATE ne $WAKESTATE ]];
+if [[ "$STATE" = "$WAKESTATE" ]]
+then 
+	if [[ -z "$ALREADYLOGGEDINTODAY" ]] 
 	then 
-		# validation
-		osascript -e "display notification \"State is $STATE but the entries in $FILE suggest it is a wake event. Please check your $FILE\" with title \"$APPNAME\""
-		exit 1;
-	elif [[ $(( $NOW - $LASTSLEEPTIMESTAMP )) -lt THRESHOLD ]]
-	then
-		# Was not big enough to be considered a lunch break
-		logger -t $APPNAME "Removing last sleep time from $FILE"
-		sed -i '' '$ s/'$TIMESEPARATOR$LASTSLEEPTIME'//' $FILE
+		LASTWORKINGDAY=`tail -n 1 $FILE | awk -F " " '{print $1}'`
+
+		if [[ ! -z $LASTWORKINGDAY ]]
+		then
+			LASTSHUTDOWNTIMESTAMP=`tail -r /private/var/log/system.log | grep -m 1 $SHUTDOWNPATTERN | awk -F "$SHUTDOWNPATTERN" '{print $2}' | awk '{print $1}'`
+			# an alternative way to do this would be through last shutdown | head -n 1 but too slow and the format is not suitable for date to parse
+			if [ -z $LASTSHUTDOWNTIMESTAMP ]
+			then 
+				osascript -e "display notification \"No shutdown time was found, please insert it manually in $FILE\" with title \"$APPNAME\""
+			else 
+				LASTSHUTDOWNTIME=`date -j -f "%s" $LASTSHUTDOWNTIMESTAMP "+%T"` 
+				if [[ -z `tail -n 1 $FILE | grep $LASTSHUTDOWNTIME` ]] 
+				then
+					# insert it only if it's not entered already
+					logger -t $APPNAME "Writing last shutdown time to $FILE"
+					sed -i '' '$ s/$/'$TIMESEPARATOR$LASTSHUTDOWNTIME'/' $FILE
+				fi
+			fi
+
+			sleep 2 # give some time for the notifications
+			# Create results for previous working day	
+			# showSummary
+		else
+			# the script starts with a fresh FILE
+			osascript -e "display notification \"$APPNAME has started recording...\" with title \"Ahoy!\""
+		fi
+
+		logger -t $APPNAME "Starting a new day"
+		printf "%s %s" "$TODAY" "$TIME" >> $FILE
+
+		exit 0;
 	else 
-		logger -t $APPNAME "Writing new wake time to $FILE"
-		sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE
-	
-		sleep 2;	
-		osascript -e "display notification \"Resuming recording...\" with title \"$APPNAME\""
+		if [ ! -z "$LASTSLEEPTIME" ]
+		then
+			LASTSLEEPTIMESTAMP=`date -j -f "%T" "${LASTSLEEPTIME}" "+%s"`
+			
+			if [[ $(( $NOW - $LASTSLEEPTIMESTAMP )) -lt THRESHOLD ]]
+			then
+				# Was not big enough to be considered a lunch break
+				logger -t $APPNAME "Removing last sleep time from $FILE"
+				sed -i '' '$ s/'$TIMESEPARATOR$LASTSLEEPTIME'//' $FILE
+			else 
+				logger -t $APPNAME "Writing new wake time to $FILE"
+				sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE
+			
+				sleep 2;	
+				osascript -e "display notification \"Resuming recording...\" with title \"$APPNAME\""
+			fi
+		else 
+			# validation
+			osascript -e "display notification \"State is $STATE but the entries in $FILE suggest it is a sleep event. Please check your $FILE\" with title \"$APPNAME\""
+			exit 1;
+		fi
 	fi
 else 
-then
-	if [[ $STATE ne $SLEEPSTATE ]]
+	# we are sleeping
+	if [[ -z $ALREADYLOGGEDINTODAY || ! -z "$LASTSLEEPTIME" ]] 
 	then
-		# validation again
-		osascript -e "display notification \"State is $STATE but the entries in $FILE suggest it is a sleep event. Please check your $FILE\" with title \"$APPNAME\""
+		# validation
+		osascript -e "display notification \"State is $STATE but the entries in $FILE suggest it should be a wake event. Please check your $FILE\" with title \"$APPNAME\""
 		exit 1;
 	elif [[ "`isItLunchTime $NOW`" = true ]]
 	then
 		logger -t $APPNAME "Writing sleep time to $FILE"
 		sed -i '' '$ s/$/'$TIMESEPARATOR$TIME'/' $FILE # printf "%s%s" "$TIMESEPARATOR" "$TIME" >> $FILE
+	else
+		logger -t $APPNAME "Skipping sleep time, not into lunch limits"
 	fi
 fi
 )
